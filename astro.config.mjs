69b920c14cd0164638d2d100ba323c42d6c227d0
rubenvs <ruben.van.steenberghe@student.howest.be>
2025-11-19 11:54:59 +0100

    changeed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rubenlydius.github.io',
-  base: '/cck/',
+  base: '/githubpagesastro2/',
 });
